ee42815b0bf8efa2864569352693ba1588ba8e5d
flanegociosonline-cell <flanegociosonline@gmail.com>
2025-11-06 10:52:48 -0300

    refresh

--- docs/assets/js/scripts.js
+++ docs/assets/js/scripts.js
@@ -31,3 +31,18 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log('Cookies recusados.');
     });
 });
+
+
+// toogle
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const mainNav = document.getElementById('main-nav');
+
+    if (menuToggle && mainNav) {
+        menuToggle.addEventListener('click', () => {
+            const isExpanded = menuToggle.getAttribute('aria-expanded') === 'true' || false;
+            menuToggle.setAttribute('aria-expanded', !isExpanded);
+            mainNav.classList.toggle('is-open');
+        });
+    }
+});
